31ef741da4d4ec0d135d04e841bda429ad7922ff
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-14 09:32:15 +0700

    Update CalculatorTest.java

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,3 +1,37 @@
+// import org.junit.Test;
+// import static org.junit.Assert.assertEquals;
+
+// public class Calculator {
+//     public int add(int a, int b) {
+//         return a + b;
+//     }
+
+//     public static void main(String[] args) {
+//         Calculator calculator = new Calculator();
+//         int a = 4; // Thay đổi giá trị từ 3 thành 4
+//         int b = 6; // Thay đổi giá trị từ 5 thành 6
+//         int sum = calculator.add(a, b);
+//         System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+//     }
+
+//     @Test
+//     public void testAddPass() {
+//         Calculator calculator = new Calculator();
+//         int result = calculator.add(4, 6); // Thay đổi giá trị từ 3 và 5 thành 4 và 6
+//         // Kết quả mong đợi là 10, nếu bằng nhau thì test pass
+//         assertEquals(10, result);
+//     }
+
+//     @Test
+//     public void testAddFail() {
+//         Calculator calculator = new Calculator();
+//         int result = calculator.add(4, 6); // Thay đổi giá trị từ 3 và 5 thành 4 và 6
+//         // Kết quả mong đợi là 11 (sai), sẽ dẫn đến việc test fail
+//         assertEquals(11, result);
+//     }
+// }
+
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -8,8 +42,8 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 4; // Thay đổi giá trị từ 3 thành 4
-        int b = 6; // Thay đổi giá trị từ 5 thành 6
+        int a = 3; // Sửa giá trị thành 3
+        int b = 4; // Sửa giá trị thành 4
         int sum = calculator.add(a, b);
         System.out.println("Sum of " + a + " and " + b + " is: " + sum);
     }
@@ -17,16 +51,16 @@ public class Calculator {
     @Test
     public void testAddPass() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(4, 6); // Thay đổi giá trị từ 3 và 5 thành 4 và 6
-        // Kết quả mong đợi là 10, nếu bằng nhau thì test pass
-        assertEquals(10, result);
+        int result = calculator.add(3, 4); // Sửa giá trị từ 4 và 6 thành 3 và 4
+        // Kết quả mong đợi là 7, nếu bằng nhau thì test pass
+        assertEquals(7, result);
     }
 
     @Test
     public void testAddFail() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(4, 6); // Thay đổi giá trị từ 3 và 5 thành 4 và 6
-        // Kết quả mong đợi là 11 (sai), sẽ dẫn đến việc test fail
-        assertEquals(11, result);
+        int result = calculator.add(3, 4); // Sửa giá trị từ 4 và 6 thành 3 và 4
+        // Kết quả mong đợi là 8 (sai), sẽ dẫn đến việc test fail
+        assertEquals(8, result);
     }
 }
